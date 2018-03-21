f5a84c932a00add8dc90acc1cdb69dc3967949d7
Greg Albiston <greg_albiston@hotmail.com>
2018-03-21 17:33:51 +0000

    Initial project setup.

---
+++ src/main/java/geosparql_benchmarking/Main.java
@@ -0,0 +1,22 @@
+package geosparql_benchmarking;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Run GeoSPARQL Compliance Testing
+        //1) Serialisation - WKT, GML, mixed
+        //2) RDFS Entailment
+        //3) Function coverage
+        //4) Property retrieval and generation
+        //5) Units of measure in function - conversion
+        //6) Coordinate reference system - conversion
+        //Run Geographica Benchmarks
+        //1) Apache Jena Extension
+        //2) Parliament
+        //3) Strabon
+    }
+}
