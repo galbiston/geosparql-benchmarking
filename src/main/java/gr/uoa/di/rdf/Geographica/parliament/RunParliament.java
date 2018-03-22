ab99598f2f46ec83bde2165a5d5a9711147fe6d8
Greg Albiston <greg_albiston@hotmail.com>
2018-03-22 16:59:58 +0000

    RunParliment: added override annotation.

--- src/main/java/gr/uoa/di/rdf/Geographica/parliament/RunParliament.java
+++ src/main/java/gr/uoa/di/rdf/Geographica/parliament/RunParliament.java
@@ -1,30 +1,29 @@
 package gr.uoa.di.rdf.Geographica.parliament;
 
 import gr.uoa.di.rdf.Geographica.systemsundertest.RunSystemUnderTest;
-
 import org.apache.log4j.Logger;
 
 public class RunParliament extends RunSystemUnderTest {
-	static Logger logger = Logger
-			.getLogger(RunParliament.class.getSimpleName());
 
-	// @Override
-	// protected void addOptions() {
-	// super.addOptions();
-	// }
+    static Logger logger = Logger
+            .getLogger(RunParliament.class.getSimpleName());
 
-	// @Override
-	// protected void logOptions() {
-	// super.logOptions();
-	// }
+    // @Override
+    // protected void addOptions() {
+    // super.addOptions();
+    // }
+    // @Override
+    // protected void logOptions() {
+    // super.logOptions();
+    // }
+    @Override
+    protected void initSystemUnderTest() throws Exception {
+        sut = new ParliamentSUT();
+    }
 
-	protected void initSystemUnderTest() throws Exception {
-		sut = new ParliamentSUT();
-	}
+    public static void main(String[] args) throws Exception {
+        RunSystemUnderTest runParliament = new RunParliament();
 
-	public static void main(String[] args) throws Exception {
-		RunSystemUnderTest runParliament = new RunParliament();
-		
-		runParliament.run(args);
-	}
+        runParliament.run(args);
+    }
 }
