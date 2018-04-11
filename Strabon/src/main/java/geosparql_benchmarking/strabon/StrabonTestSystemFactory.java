eca01356ba8c092b2c1594a30e30d4b52da71c69
Greg Albiston <greg_albiston@hotmail.com>
2018-04-11 14:22:30 +0100

    StrabonTestSystemFactory: database dropping and creation methods added.

--- Strabon/src/main/java/geosparql_benchmarking/strabon/StrabonTestSystemFactory.java
+++ Strabon/src/main/java/geosparql_benchmarking/strabon/StrabonTestSystemFactory.java
@@ -12,11 +12,13 @@ import geosparql_benchmarking.experiments.DatasetLoadTimeResult;
 import geosparql_benchmarking.experiments.TestSystem;
 import geosparql_benchmarking.experiments.TestSystemFactory;
 import java.io.File;
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +30,7 @@ public class StrabonTestSystemFactory implements TestSystemFactory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private final String db;
+    private final String dbName;
     private final String user;
     private final String password;
     private final Integer port;
@@ -38,10 +40,12 @@ public class StrabonTestSystemFactory implements TestSystemFactory {
     private final String postgresDataPath;
     private final String postgresIsReadyPath;
     private final String postgresPG_CTLPath;
+    private final String postgresCreateDBPath;
+    private final String postgresDropDBPath;
     public static final String TEST_SYSTEM_NAME = "Strabon";
 
-    public StrabonTestSystemFactory(String db, String user, String password, Integer port, String host, String resultsFolder, String postgresBinPath, String postgresDataPath) {
-        this.db = db;
+    public StrabonTestSystemFactory(String dbName, String user, String password, Integer port, String host, String resultsFolder, String postgresBinPath, String postgresDataPath) {
+        this.dbName = dbName;
         this.user = user;
         this.password = password;
         this.port = port;
@@ -54,15 +58,19 @@ public class StrabonTestSystemFactory implements TestSystemFactory {
         if (postgresBinPath.isEmpty()) {
             this.postgresIsReadyPath = "pg_isready";
             this.postgresPG_CTLPath = "pg_ctl";
+            this.postgresCreateDBPath = "createDB";
+            this.postgresDropDBPath = "dropDB";
         } else {
             this.postgresIsReadyPath = postgresBinPath + "pg_isready\"";
             this.postgresPG_CTLPath = postgresBinPath + "pg_ctl\"";
+            this.postgresCreateDBPath = postgresBinPath + "createDB\"";
+            this.postgresDropDBPath = postgresBinPath + "dropDB\"";
         }
         this.postgresDataPath = postgresDataPath;
     }
 
-    public String getDb() {
-        return db;
+    public String getDbName() {
+        return dbName;
     }
 
     public String getUser() {
@@ -97,6 +105,14 @@ public class StrabonTestSystemFactory implements TestSystemFactory {
         return postgresPG_CTLPath;
     }
 
+    public String getPostgresCreateDBPath() {
+        return postgresCreateDBPath;
+    }
+
+    public String getPostgresDropDBPath() {
+        return postgresDropDBPath;
+    }
+
     @Override
     public TestSystem getTestSystem() {
         return getStrabonTestSystem();
@@ -104,7 +120,7 @@ public class StrabonTestSystemFactory implements TestSystemFactory {
 
     public StrabonTestSystem getStrabonTestSystem() {
         try {
-            return new StrabonTestSystem(db, user, password, port, host, postgresIsReadyPath, postgresPG_CTLPath, postgresDataPath);
+            return new StrabonTestSystem(dbName, user, password, port, host, postgresIsReadyPath, postgresPG_CTLPath, postgresDataPath);
         } catch (Exception ex) {
             LOGGER.error("Strabon Exception: {}", ex.getMessage());
             throw new AssertionError("Strabon failed to initialise.");
@@ -128,13 +144,13 @@ public class StrabonTestSystemFactory implements TestSystemFactory {
         long startNanoTime = System.nanoTime();
         Strabon strabon = null;
         try {
-            String db = testSystemFactory.getDb();
+            String dbName = testSystemFactory.getDbName();
             String user = testSystemFactory.getUser(); //String user = "postgres";
             String password = testSystemFactory.getPassword(); //String passwd = "postgres";
             Integer port = testSystemFactory.getPort();
             String host = testSystemFactory.getHost(); //"localhost"; //"127.0.0.1"
             Boolean checkForLockTable = true;
-            strabon = new Strabon(db, user, password, port, host, checkForLockTable);
+            strabon = new Strabon(dbName, user, password, port, host, checkForLockTable);
 
             for (Map.Entry<String, File> entry : datasetMap.entrySet()) {
 
@@ -160,6 +176,37 @@ public class StrabonTestSystemFactory implements TestSystemFactory {
         long endNanoTime = System.nanoTime();
         LOGGER.info("Strabon Loading: Completed");
         return new DatasetLoadResult(TEST_SYSTEM_NAME, isCompleted, startNanoTime, endNanoTime, datasetLoadTimeResults);
+    }
+
+    public void createPostgresDatabase(String template) throws IOException, InterruptedException {
+        String[] postgresCreate = new String[]{postgresCreateDBPath, "-T", template, "-h", host, "-p", port.toString(), "-U", user, dbName};
+
+        Process pr = Runtime.getRuntime().exec(postgresCreate);
+        int createResult = pr.waitFor();
+        if (createResult > 0) {
+            String createCommand = StringUtils.join(postgresCreate, " ");
+            LOGGER.error("PostgreSQL failed to createDB: Exit Value - {}. Absolute path to PostgreSQL bin and data folders may be required. Postgres start command: {}", createResult, createCommand);
+        } else {
+            if (Main.DEBUG_MESSAGES) {
+                LOGGER.info("Postgres createdDB: {}", dbName);
+            }
+        }
+    }
+
+    public void dropPostgresDatabase() throws IOException, InterruptedException {
+
+        String[] postgresCreate = new String[]{postgresDropDBPath, "-h", host, "-p", port.toString(), "-U", user, "--if-exists", dbName};
+
+        Process pr = Runtime.getRuntime().exec(postgresCreate);
+        int createResult = pr.waitFor();
+        if (createResult > 0) {
+            String createCommand = StringUtils.join(postgresCreate, " ");
+            LOGGER.error("PostgreSQL failed to dropDB: Exit Value - {}. Absolute path to PostgreSQL bin and data folders may be required. Postgres start command: {}", createResult, createCommand);
+        } else {
+            if (Main.DEBUG_MESSAGES) {
+                LOGGER.info("Postgres droppedDB: {}", dbName);
+            }
+        }
     }
 
 }
