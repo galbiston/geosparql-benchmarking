00ffaf6fba10af350d62a4dcf25237de4fe38270
Greg Albiston <greg_albiston@hotmail.com>
2018-05-14 12:46:12 +0100

    GeoSPARQLJenaBenchmark: Main test methods.

--- GeoSPARQLJenaBenchmark/src/main/java/geosparql_benchmarking/geosparql_jena/Main.java
+++ GeoSPARQLJenaBenchmark/src/main/java/geosparql_benchmarking/geosparql_jena/Main.java
@@ -9,14 +9,20 @@ import data_setup.Dataset_WGS84_Legacy;
 import data_setup.GraphURI;
 import execution.BenchmarkExecution;
 import execution.QueryCase;
+import execution.QueryLoader;
+import execution.TestSystem;
 import execution.TestSystemFactory;
+import execution_results.QueryResult;
+import implementation.GeoSPARQLSupport;
 import implementation.data_conversion.ConvertCRS;
 import implementation.data_conversion.GeoSPARQLPredicates;
+import implementation.support.GeoSerialisationEnum;
 import implementation.vocabulary.SRS_URI;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
@@ -35,6 +41,8 @@ import org.apache.jena.tdb.TDBFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import queries.geographica.MicroBenchmark;
+import static queries.geographica.MicroBenchmark.GIVEN_POLYGON;
+import static queries.geographica.MicroBenchmark.SPATIAL_SELECTIONS;
 
 public class Main {
 
@@ -54,7 +62,9 @@ public class Main {
         TreeMap<String, File> datasetMap = Dataset_WGS84.getAll();
 
         Boolean inferenceEnabled = true;
-
+        //equalsTest();
+        //runPartsTDB(inferenceEnabled);
+        //test();
         //convertDatasetCRS();
         //applyPredicates();
         //indexInMemoryTest();
@@ -62,7 +72,7 @@ public class Main {
         //TDB
         //GeosparqlJenaTDBTestSystemFactory.clearDataset(GEOSPARQL_JENA_TDB_FOLDER);
         //GeosparqlJenaTDBTestSystemFactory.loadDataset(GEOSPARQL_JENA_TDB_FOLDER, datasetMap, inferenceEnabled);
-        //runTDB(inferenceEnabled);
+        runTDB(inferenceEnabled);
         //runTestTDB(inferenceEnabled);
         //rdfsJenaTDBTest();
         //In Memory
@@ -75,16 +85,40 @@ public class Main {
         //runDatasetLoad(tdbTestSystemFactory, BenchmarkParameters.ITERATIONS, datasetMap);
         //runDatasetLoad(memTestSystemFactory, BenchmarkParameters.ITERATIONS, datasetMap);
         //runDatasetLoad(noIndexTestSystemFactory, BenchmarkParameters.ITERATIONS, datasetMap);
-        //GeosparqlJenaMemTestSystemFactory.loadDataset(Dataset_WGS84_Legacy.getLinkedGeodata(), inferenceEnabled, memDataset);
+        //GeosparqlJenaMemTestSystemFactory.loadDataset(datasetMap, inferenceEnabled, memDataset);
         //rdfsJenaMemTest(memDataset);
         //generateGeonamesFile();
         //generatePoints();
     }
 
+    public static void runPartsTDB(Boolean inferenceEnabled) {
+        GeosparqlJenaTDBTestSystemFactory testSystemFactory = new GeosparqlJenaTDBTestSystemFactory(GEOSPARQL_JENA_TDB_FOLDER, GEOSPARL_JENA_TDB_RESULTS_FOLDER_NAME, inferenceEnabled);
+        List<QueryCase> queryCases = new ArrayList<>();
+        queryCases.add(new QueryCase("Query16", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query16.spl").replace("GIVEN_POLYGON_IN_WKT", GIVEN_POLYGON)));
+        queryCases.add(new QueryCase("Query17", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query17.spl").replace("GIVEN_POLYGON_IN_WKT", GIVEN_POLYGON)));
+
+        /*
+        queryCases.add(new QueryCase("Query7", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query7.spl").replace("GIVEN_LINE_IN_WKT", GIVEN_LINESTRING_1)));
+        queryCases.add(new QueryCase("Query8", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query8.spl").replace("GIVEN_POLYGON_IN_WKT", GIVEN_POLYGON)));
+        queryCases.add(new QueryCase("Query9", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query9.spl").replace("GIVEN_POLYGON_IN_WKT", GIVEN_POLYGON)));
+        queryCases.add(new QueryCase("Query10", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query10.spl").replace("GIVEN_LINE_IN_WKT", GIVEN_LINESTRING_2)));
+        queryCases.add(new QueryCase("Query11", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query11.spl").replace("GIVEN_POLYGON_IN_WKT", GIVEN_POLYGON)));
+        queryCases.add(new QueryCase("Query12", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query12.spl").replace("GIVEN_LINE_IN_WKT", GIVEN_LINESTRING_3)));
+        queryCases.add(new QueryCase("Query13", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query13.spl").replace("GIVEN_POLYGON_IN_WKT", GIVEN_POLYGON)));
+        queryCases.add(new QueryCase("Query14", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query14.spl").replace("GIVEN_POINT_IN_WKT", GIVEN_POINT).replace("GIVEN_RADIUS", GIVEN_RADIUS)));
+        queryCases.add(new QueryCase("Query15", "SpatialSelections", QueryLoader.readFile(SPATIAL_SELECTIONS + "/Query15.spl").replace("GIVEN_POINT_IN_WKT", GIVEN_POINT).replace("GIVEN_RADIUS", GIVEN_RADIUS)));
+        queryCases.add(new QueryCase("Query19", "SpatialJoins", QueryLoader.readFile(SPATIAL_JOINS + "/Query19.spl")));
+        queryCases.add(new QueryCase("Query21", "SpatialJoins", QueryLoader.readFile(SPATIAL_JOINS + "/Query21.spl")));
+        queryCases.add(new QueryCase("Query23", "SpatialJoins", QueryLoader.readFile(SPATIAL_JOINS + "/Query23.spl")));
+        queryCases.add(new QueryCase("Query25", "SpatialJoins", QueryLoader.readFile(SPATIAL_JOINS + "/Query25.spl")));
+        queryCases.add(new QueryCase("Query27", "SpatialJoins", QueryLoader.readFile(SPATIAL_JOINS + "/Query27.spl")));
+         */
+        BenchmarkExecution.runBoth(testSystemFactory, BenchmarkParameters.ITERATIONS, BenchmarkParameters.TIMEOUT, queryCases, BenchmarkParameters.RESULT_LINE_LIMIT_ZERO);
+    }
+
     public static void runTDB(Boolean inferenceEnabled) {
         GeosparqlJenaTDBTestSystemFactory testSystemFactory = new GeosparqlJenaTDBTestSystemFactory(GEOSPARQL_JENA_TDB_FOLDER, GEOSPARL_JENA_TDB_RESULTS_FOLDER_NAME, inferenceEnabled);
         BenchmarkExecution.runBoth(testSystemFactory, BenchmarkParameters.ITERATIONS, BenchmarkParameters.TIMEOUT, MicroBenchmark.loadMainQuerySet(), BenchmarkParameters.RESULT_LINE_LIMIT_ZERO);
-
     }
 
     public static void runInMemory(TreeMap<String, File> datasetMap, Boolean inferenceEnabled) {
@@ -255,6 +289,62 @@ public class Main {
 
     public static void generatePoints() {
         DataGeneration.generateGeographicaPoint(100, new File("points.txt"));
+    }
+
+    public static void test() {
+
+        Dataset dataset = TDBFactory.createDataset(GEOSPARQL_JENA_TDB_FOLDER.getAbsolutePath());
+        GeoSPARQLSupport.loadFunctionsMemoryIndex();
+        String queryString = "PREFIX geof: <http://www.opengis.net/def/function/geosparql/>\n"
+                + "PREFIX datasets: <http://geographica.di.uoa.gr/dataset/>                           \n"
+                + "PREFIX geonames: <http://www.geonames.org/ontology#>                               \n"
+                + "PREFIX opengis: <http://www.opengis.net/def/uom/OGC/1.0/>\n"
+                + "                                                                                   \n"
+                + "SELECT (geof:buffer(?o1, 4, opengis:metre) AS ?ret) \n"
+                + "WHERE {                                                                            \n"
+                + "    GRAPH datasets:geonames {?s1 geonames:asWKT ?o1}\n"
+                + "}";
+
+        dataset.begin(ReadWrite.READ);
+        try (QueryExecution qe = QueryExecutionFactory.create(queryString, dataset)) {
+            ResultSet rs = qe.execSelect();
+            ResultSetFormatter.outputAsCSV(rs);
+        }
+        dataset.end();
+        dataset.close();
+
+    }
+
+    private static void equalsTest() {
+
+        GeosparqlJenaTDBTestSystemFactory testSystemFactory = new GeosparqlJenaTDBTestSystemFactory(GEOSPARQL_JENA_TDB_FOLDER, GEOSPARL_JENA_TDB_RESULTS_FOLDER_NAME, true);
+
+        String queryString = "PREFIX geof: <http://www.opengis.net/def/function/geosparql/> "
+                + "SELECT ?res WHERE{"
+                + "BIND(\"<http://www.opengis.net/def/crs/EPSG/0/2100> POINT (474382.14862145175 4203347.7258966705)\"^^<http://www.opengis.net/ont/geosparql#wktLiteral> AS ?first)"
+                + "BIND(\"<http://www.opengis.net/def/crs/EPSG/0/4326> POINT(37.98 23.71)\"^^<http://www.opengis.net/ont/geosparql#wktLiteral> AS ?second)"
+                + "BIND(geof:sfEquals(?first, ?second) AS ?res) "
+                + "}";
+
+        try (TestSystem testSystem = testSystemFactory.getTestSystem()) {
+            QueryResult qResult = testSystem.runQueryWithTimeout(queryString, Duration.ofHours(1));
+            System.out.println(qResult.getResults());
+
+        } catch (Exception ex) {
+            LOGGER.error("Exception: {}", ex.getMessage());
+        }
+
+    }
+
+    public void convertCRS() {
+        System.out.println("convertCRS");
+
+        String geometryLiteral = "<http://www.opengis.net/def/crs/EPSG/0/4326> POINT(37.98 23.71)";
+        String outputSrsURI = "http://www.opengis.net/def/crs/EPSG/0/2100";
+
+        String convertedGeometryLiteral = ConvertCRS.convertGeometryLiteral(geometryLiteral, outputSrsURI, GeoSerialisationEnum.WKT);
+        System.out.println("Original: " + geometryLiteral);
+        System.out.println("Conversion: " + convertedGeometryLiteral);
     }
 
 }
