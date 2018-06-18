d2325f2feb52143f0fd59c627cb98cfccc5a648c
Greg Albiston <greg_albiston@hotmail.com>
2018-06-18 19:51:34 +0100

    - additional script.

--- GeoSPARQLJenaBenchmark/runUserTestQuery.sh
+++ GeoSPARQLJenaBenchmark/runUserTestQuery.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gradle run -PappArgs="['tdb','../user_defined_macro_test','cold', '1']"
+gradle run -PappArgs="['tdb','../user_defined_macro_test','cold', '1', '3600', '5000']"
---
+++ StrabonBenchmark/runUserTestQuery.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gradle run -PappArgs="['../user_defined_macro_test','cold', '1', '3600', '5000']"
